6cc41c4725ac82ba1b9744af781187647b581f0a
luo <674498248@qq.com>
2024-05-13 14:27:13 +0800

    改bug，修改KMCCC识别不了高版本JAVA的问题

--- ST_Laucher/Views/MainView.axaml.cs
+++ ST_Laucher/Views/MainView.axaml.cs
@@ -1,11 +1,59 @@
 ﻿using Avalonia.Controls;
-
+using Avalonia.Interactivity;
+using System.Diagnostics;
+using KMCCC;
+using ST_Laucher.laucherCore.tools;
+using Microsoft.Win32;
+using System.Collections;
+using System.Linq;
+using KMCCC.Authentication;
+using KMCCC.Launcher;
+using System.Runtime.ConstrainedExecution;
+using System.Collections.Generic;
 namespace ST_Laucher.Views;
 
 public partial class MainView : UserControl
 {
+    public static IEnumerable<string> java_ver;
+    public static KMCCC.Launcher.Version[] versions;
+    KMCCC.Launcher.LauncherCore core = KMCCC.Launcher.LauncherCore.Create();
     public MainView()
     {
         InitializeComponent();
+        //获取java版本
+        java_ver = SystemTools.FindJava();
+        //获取版本
+        versions = core.GetVersions().ToArray();
+        //导入前端
+        versionsList.ItemsSource = versions;
+        javaList.ItemsSource = java_ver;
+
+        if(versions.Length == 0) { Debug.WriteLine("没有版本"); }
+        foreach (var version in versions)
+        {
+            Debug.WriteLine(version.ToString());
+        }
+        foreach (var version in java_ver)
+        {
+            Debug.WriteLine(version.ToString());
+        }
     }
+
+    public void start(object source, RoutedEventArgs args)
+    {
+        Debug.WriteLine(javaList.SelectedItem);
+        core.JavaPath = (string)javaList.SelectedItem;
+        var ver = (KMCCC.Launcher.Version)versionsList.SelectedItem;
+        
+        var result = core.Launch(new KMCCC.Launcher.LaunchOptions
+        {
+            Version = ver,
+            MaxMemory = 2048,
+            Authenticator = new OfflineAuthenticator("YOUR_FATHER"),
+            Mode = LaunchMode.MCLauncher,
+            Size = new WindowSize{Height = 768, Width = 1280}
+        });
+        Debug.WriteLine(result.ToString);
+    }
+    
 }
---
+++ ST_Laucher/laucherCore/tools/SystemTools.cs
@@ -0,0 +1,84 @@
+﻿using DynamicData;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST_Laucher.laucherCore.tools
+{
+    public class SystemTools
+    {
+        public static IEnumerable<String> FindJava()
+        {
+            try {
+                var rootReg = Registry.LocalMachine.OpenSubKey("SOFTWARE");
+                return rootReg == null ?
+                    new string[0]
+                    : FindJavaInternal(rootReg);
+            }
+            catch { 
+                    return new string[0];
+            }
+        }
+        public static IEnumerable<string> FindJavaInternal(RegistryKey registry)
+        {
+            try
+            {
+                var registryKey = registry.OpenSubKey("JavaSoft");
+                var registryKey1 = registry.OpenSubKey("JavaSoft");
+                var registryKey2 = registry.OpenSubKey("JavaSoft");
+                var registry1 = registry;
+                var registry2 = registry;
+                //打开子表
+                if (registryKey != null)
+                {
+                    if (registryKey1.OpenSubKey("Java Runtime Environment") == null &&  registryKey2.OpenSubKey("JDK") == null) return new string[0];
+                    else
+                    {
+                        registry1 = registryKey1.OpenSubKey("Java Runtime Environment");
+                        registry2 = registryKey2.OpenSubKey("JDK");
+                        Debug.WriteLine(registry1);
+                        Debug.WriteLine(registry2);
+                    }
+                }
+                else
+                {
+                    return new string[0];
+                }
+               var below1_8 = from ver in registry1.GetSubKeyNames()
+                              select registry1.OpenSubKey(ver)
+               into command
+                              where command != null
+                              select command.GetValue("JavaHome")
+               into javaHomes
+                              where javaHomes != null
+                              select javaHomes.ToString()
+               into str
+                              where !String.IsNullOrWhiteSpace(str)
+                              select str + @"\bin\javaw.exe";
+              var up1_8 = from ver in registry2.GetSubKeyNames()
+                              select registry2.OpenSubKey(ver)
+               into command
+                              where command != null
+                              select command.GetValue("JavaHome")
+               into javaHomes
+                              where javaHomes != null
+                              select javaHomes.ToString()
+               into str
+                              where !String.IsNullOrWhiteSpace(str)
+                              select str + @"\bin\javaw.exe";
+
+
+                return below1_8.Union(up1_8);
+            }
+
+            catch
+            {
+                return new string[0];
+            }
+        }
+    }
+}
